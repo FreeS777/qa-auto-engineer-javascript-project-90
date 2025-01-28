d58bf9e54289f9a913c71d66f9445cfd16ca9490
Artem Smirnov <frees.comp@gmail.com>
2025-01-28 12:03:14 +0200

    test app render

---
+++ tests/app.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('App render', async ({ page }) => {
+  await page.goto('/');
+
+  const userName = page.getByRole('textbox', { name: /username/i });
+  await expect(userName).toBeVisible();
+
+  const password = page.getByRole('textbox', { name: /password/i });
+  await expect(password).toBeVisible();
+  const btn = page.getByRole('button', { name: /Sign/i });
+  await expect(btn).toBeVisible();
+});
